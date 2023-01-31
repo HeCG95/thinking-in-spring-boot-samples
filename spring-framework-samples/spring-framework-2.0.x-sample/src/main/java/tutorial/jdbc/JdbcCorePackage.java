143ec331427dd06d0073096ffaa60a1a3bd7efb6
HeCG95 <jacktanger@163.com>
2023-01-31 22:06:46 +0800

    [M] JDBC core package usage 2023-1-31 22:06:42

--- spring-framework-samples/spring-framework-2.0.x-sample/src/main/java/tutorial/jdbc/JdbcCorePackage.java
+++ spring-framework-samples/spring-framework-2.0.x-sample/src/main/java/tutorial/jdbc/JdbcCorePackage.java
@@ -1,12 +1,12 @@
 package tutorial.jdbc;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.*;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
+import java.util.ListIterator;
 
 public class JdbcCorePackage {
 
@@ -44,11 +44,96 @@ public class JdbcCorePackage {
         }
     }
 
+    public void dynamicInsert(){
+        // Insert one row
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            private String sql = "insert into specialty values(?, ?)";
+            public String getSql() { return sql; }
+            public PreparedStatement createPreparedStatement(Connection conn)
+                    throws SQLException {
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setString(1, "INTERN");
+                ps.setString(2, "Internal Medicine");
+                return ps;
+            }
+        };
+
+        int numRows = tpl.update(psc);
+        if (1 == numRows) {
+            System.out.println("One row was inserted. Rows in table are:");
+            // Need a RowCallbackHandler
+            tpl.query("select * from specialty",
+                    new RowCallbackHandler() {
+                        public void processRow(ResultSet rs) throws SQLException {
+                            System.out.println("Specialty code = " +
+                                    rs.getString(1) + ", specialty name = " + rs.getString(2));
+                        }
+                    }
+            );
+
+        }
+    }
+
+    public void dynamicInsertUsingPSFactory(){
+        // Insert one row
+        int[] types = new int[] { Types.VARCHAR, Types.VARCHAR };
+        Object[] params = new Object[] { new String("RESP"),
+                new String("Respiratory Diseases") };
+        PreparedStatementCreator psc =
+                new PreparedStatementCreatorFactory("insert into specialty values(?, ?)", types).
+                        newPreparedStatementCreator(params);
+//                PreparedStatementCreatorFactory.newPreparedStatementCreator("insert into specialty values(?, ?)", types, params);
+
+        int numRows = tpl.update(psc);
+        if (1 == numRows) {
+            System.out.println("One row was inserted. Rows in table are:");
+            // Need a RowCallbackHandler
+            tpl.query("select * from specialty",
+                    new RowCallbackHandler() {
+                        public void processRow(ResultSet rs) throws SQLException {
+                            System.out.println("Specialty code = " +
+                                    rs.getString(1) + ", specialty name = " + rs.getString(2));
+                        }
+                    }
+            );
+
+        }
+    }
+
+    /**
+     * ResultReader 换成了 RowMapper
+     */
+    public void readAllRows(){
+//        new
+        RowMapper reader = new RowMapper() {
+            @Override
+            public Object mapRow(ResultSet rs, int i) throws SQLException {
+                Specialty spec = new Specialty();
+                spec.setCode(rs.getString(1));
+                spec.setDescription(rs.getString(2));
+                return spec;
+            }
+        };
+
+        List list = tpl.query("select * from specialty", reader);
+        System.out.println("Listing all rows");
+        ListIterator iter = list.listIterator();
+        while (iter.hasNext()) {
+            Specialty spec = (Specialty)(iter.next());
+            System.out.println("Specialty code = " +
+                    spec.getCode() + ", specialty name = " +
+                    spec.getDescription());
+        }
+    }
+
     public static void main(String[] args) {
 
         JdbcCorePackage demo = new JdbcCorePackage();
-        demo.setUp();
-        demo.staticInsert();
+//        demo.setUp();
+//        demo.staticInsert();
+//        demo.dynamicInsert();
+//        demo.dynamicInsertUsingPSFactory();
+        demo.readAllRows();
 
     }
 
